1586fa21b46f777bbebc8d40ab6652b1be86e3c8
Shone Jacob <shone.jacob@gmail.com>
2019-01-04 19:46:05 -0500

    Code Updated

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # homebrew-tools
 Tools created in HomeBrew for MacOS
+
+Installation
+
+```
+brew tap shoneslab/tools
+brew install fission-cli
+```
+
+Check the version
+```
+fission --version
+client:
+  fission/core:
+    gitcommit: 045ab734c6bd04cdc07ebd80f05874c7122ffd18
+    builddate: 2018-12-13T07:56:27Z
+    version: 1.0-rc1
+server:
+  fission/core:
+    gitcommit: 045ab734c6bd04cdc07ebd80f05874c7122ffd18
+    builddate: 2018-12-13T07:56:27Z
+    version: 1.0-rc1
+```
--- fission-cli.rb
+++ fission-cli.rb
@@ -1,13 +1,13 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class FissionCli < Formula
   desc "Fission CLI installation through homebrew"
-  homepage ""
-  url "https://github.com/shoneslab/fission-cli/releases/download/0.0.4/fission.tar"
-  sha256 "8025b9c256a2700f87eeebfe742db2df0a1041e87c8b6d7af578430708350ad5"
+  homepage "https://github.com/shoneslab/fission-cli"
+  url "https://github.com/shoneslab/fission-cli/releases/download/0.0.6/fission.tar"
+  sha256 "df81c130e42f37928b21cc2b085c86e03cfa7a93e1a64540fa15caeaed0a962b"
 
   def install
     bin.install "fission"
+  end
+
+  test do
   end
 end
